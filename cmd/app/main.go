a6b8af4b8f2d6aa27ac0d630017d81c2d706070c
Talandar <talandar66@protonmail.com>
2026-04-17 08:31:33 +0200

    dbus

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,101 +1,160 @@
+// package main
+//
+// import (
+//
+//	"fmt"
+//	"google.golang.org/protobuf/proto"
+//	"os"
+//	pb "talandar.dev/furnace_driver/proto"
+//
+// )
+//
+//	func main() {
+//		report := &pb.SensorReport{
+//			Id: 47,
+//			Ver: &pb.Version{
+//				Major: 1,
+//				Minor: 3,
+//				Patch: 2,
+//			},
+//			Status: "ERROR",
+//			Logs: "0001 INF Sensor init OK\n" +
+//				"0042 DBG Calibration done\n" +
+//				"0187 WRN Watchdog reset\n" +
+//				"0203 ERR Adc overflow CH:2\n" +
+//				"0519 INF Firmware update 1.3.2",
+//			Data: []*pb.DataPoint{
+//				{
+//					Time:   "2026-10-04 08:00:00",
+//					Temp:   22.3,
+//					Humi:   float64Ptr(45.1),
+//					Enable: true,
+//				},
+//				{
+//					Time:   "2026-10-04 08:30:00",
+//					Temp:   21.8,
+//					Humi:   float64Ptr(47.6),
+//					Enable: false,
+//				},
+//				{
+//					Time:   "2026-10-04 09:00:00",
+//					Temp:   22.1,
+//					Enable: true,
+//					// Humi brak => odpowiednik null
+//				},
+//			},
+//		}
+//
+//		data, err := proto.Marshal(report)
+//		if err != nil {
+//			panic(err)
+//		}
+//		err = os.WriteFile("report.bin", data, 0o644)
+//		if err != nil {
+//			panic(err)
+//		}
+//
+//		fmt.Printf("Zapisano %d bajtów do report.bin\n\n", len(data))
+//
+//		raw, err := os.ReadFile("report.bin")
+//		if err != nil {
+//			panic(err)
+//		}
+//
+//		var decoded pb.SensorReport
+//		err = proto.Unmarshal(raw, &decoded)
+//		if err != nil {
+//			panic(err)
+//		}
+//
+//		printReport(&decoded)
+//	}
+//
+//	func float64Ptr(v float64) *float64 {
+//		return &v
+//	}
+//
+//	func printReport(r *pb.SensorReport) {
+//		fmt.Println("ID:", r.Id)
+//
+//		if r.Ver != nil {
+//			fmt.Printf("Version: %d.%d.%d\n", r.Ver.Major, r.Ver.Minor, r.Ver.Patch)
+//		}
+//
+//		fmt.Println("Status:", r.Status)
+//		fmt.Println("Logs:")
+//		fmt.Println(r.Logs)
+//		fmt.Println()
+//
+//		for i, dp := range r.Data {
+//			fmt.Printf("DataPoint #%d\n", i+1)
+//			fmt.Println("  Time  :", dp.Time)
+//			fmt.Println("  Temp  :", dp.Temp)
+//			fmt.Println("  Enable:", dp.Enable)
+//
+//			if dp.Humi != nil {
+//				fmt.Println("  Humi  :", *dp.Humi)
+//			} else {
+//				fmt.Println("  Humi  : <null / brak>")
+//			}
+//
+//			fmt.Println()
+//		}
+//	}
 package main
 
 import (
+	"flag"
 	"fmt"
-	"google.golang.org/protobuf/proto"
+	"log"
 	"os"
-	pb "talandar.dev/furnace_driver/proto"
+
+	"github.com/godbus/dbus/v5"
 )
 
 func main() {
-	report := &pb.SensorReport{
-		Id: 47,
-		Ver: &pb.Version{
-			Major: 1,
-			Minor: 3,
-			Patch: 2,
-		},
-		Status: "ERROR",
-		Logs: "0001 INF Sensor init OK\n" +
-			"0042 DBG Calibration done\n" +
-			"0187 WRN Watchdog reset\n" +
-			"0203 ERR Adc overflow CH:2\n" +
-			"0519 INF Firmware update 1.3.2",
-		Data: []*pb.DataPoint{
-			{
-				Time:   "2026-10-04 08:00:00",
-				Temp:   22.3,
-				Humi:   float64Ptr(45.1),
-				Enable: true,
-			},
-			{
-				Time:   "2026-10-04 08:30:00",
-				Temp:   21.8,
-				Humi:   float64Ptr(47.6),
-				Enable: false,
-			},
-			{
-				Time:   "2026-10-04 09:00:00",
-				Temp:   22.1,
-				Enable: true,
-				// Humi brak => odpowiednik null
-			},
-		},
-	}
+	title := flag.String("title", "Test notification", "notification title")
+	body := flag.String("body", "Hello from Go over D-Bus!", "notification body")
+	appName := flag.String("app", "furnace_driver", "application name")
+	flag.Parse()
 
-	data, err := proto.Marshal(report)
+	conn, err := dbus.SessionBus()
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to connect to session bus: %v", err)
 	}
-	err = os.WriteFile("report.bin", data, 0o644)
-	if err != nil {
-		panic(err)
+	defer conn.Close()
+
+	obj := conn.Object(
+		"org.freedesktop.Notifications",
+		dbus.ObjectPath("/org/freedesktop/Notifications"),
+	)
+
+	var notificationID uint32
+
+	call := obj.Call(
+		"org.freedesktop.Notifications.Notify",
+		0,
+		*appName,
+		uint32(0),
+		"",
+		*title,
+		*body,
+		[]string{},
+		map[string]dbus.Variant{},
+		int32(5000),
+	)
+	if call.Err != nil {
+		log.Fatalf("failed to send notification: %v", call.Err)
 	}
 
-	fmt.Printf("Zapisano %d bajtów do report.bin\n\n", len(data))
-
-	raw, err := os.ReadFile("report.bin")
-	if err != nil {
-		panic(err)
+	if err := call.Store(&notificationID); err != nil {
+		log.Fatalf("failed to decode notification id: %v", err)
 	}
 
-	var decoded pb.SensorReport
-	err = proto.Unmarshal(raw, &decoded)
-	if err != nil {
-		panic(err)
-	}
-
-	printReport(&decoded)
+	fmt.Printf("Notification sent, id=%d\n", notificationID)
 }
 
-func float64Ptr(v float64) *float64 {
-	return &v
-}
-
-func printReport(r *pb.SensorReport) {
-	fmt.Println("ID:", r.Id)
-
-	if r.Ver != nil {
-		fmt.Printf("Version: %d.%d.%d\n", r.Ver.Major, r.Ver.Minor, r.Ver.Patch)
-	}
-
-	fmt.Println("Status:", r.Status)
-	fmt.Println("Logs:")
-	fmt.Println(r.Logs)
-	fmt.Println()
-
-	for i, dp := range r.Data {
-		fmt.Printf("DataPoint #%d\n", i+1)
-		fmt.Println("  Time  :", dp.Time)
-		fmt.Println("  Temp  :", dp.Temp)
-		fmt.Println("  Enable:", dp.Enable)
-
-		if dp.Humi != nil {
-			fmt.Println("  Humi  :", *dp.Humi)
-		} else {
-			fmt.Println("  Humi  : <null / brak>")
-		}
-
-		fmt.Println()
-	}
+func init() {
+	log.SetFlags(0)
+	log.SetOutput(os.Stderr)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module talandar.dev/furnace_driver
 
 go 1.24.4
 
-require google.golang.org/protobuf v1.36.11 // indirect
+require (
+	github.com/godbus/dbus/v5 v5.2.2 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+)
